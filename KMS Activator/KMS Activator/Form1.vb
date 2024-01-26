a92d3bba80f60ea0a848232f8e88722428d23953
Black.Hacker <farisksa79@protonmail.com>
2024-01-26 16:40:46 +0300

    small update

--- KMS Activator/KMS Activator/Form1.vb
+++ KMS Activator/KMS Activator/Form1.vb
@@ -1,15 +1,21 @@
-﻿Imports System.Windows.Forms.VisualStyles.VisualStyleElement
-
-Public Class Form1
+﻿Public Class Form1
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        RunCommandCom("slmgr", "/ipk W269N-WFGWX-YVC9B-4J6C9-T83GX", False)
-        Threading.Thread.Sleep(10000)
-        RunCommandCom("slmgr", "/skms kms.digiboy.ir", False)
-        Threading.Thread.Sleep(10000)
-        RunCommandCom("slmgr", "/ato", False)
+        If (RunCommandCom("slmgr", "/ipk W269N-WFGWX-YVC9B-4J6C9-T83GX", False) = True) Then
+            If (RunCommandCom("slmgr", "/skms kms.digiboy.ir", False) = True) Then
+                If (RunCommandCom("slmgr", "/ato", False) = True) Then
+                    MsgBox("Windows Activated")
+                Else
+                    MsgBox("Activation Failled")
+                End If
+            Else
+                MsgBox("Activation Failled")
+            End If
+        Else
+            MsgBox("Activation Failled")
+        End If
     End Sub
 
-    Shared Sub RunCommandCom(command As String, arguments As String, permanent As Boolean)
+    Public Function RunCommandCom(command As String, arguments As String, permanent As Boolean) As Boolean
         Dim p As Process = New Process()
         Dim pi As ProcessStartInfo = New ProcessStartInfo()
         pi.Arguments = " " + If(permanent = True, "/K", "/C") + " " + command + " " + arguments
@@ -17,5 +23,13 @@ Public Class Form1
         p.StartInfo = pi
         pi.WindowStyle = ProcessWindowStyle.Hidden
         p.Start()
-    End Sub
+
+        p.WaitForExit()
+
+        If (p.ExitCode = 0) Then
+            Return True
+        Else
+            Return False
+        End If
+    End Function
 End Class
