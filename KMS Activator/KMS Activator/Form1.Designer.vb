356adc6e5f9c96d44707fe338b53813d096ab837
Black.Hacker <farisksa79@protonmail.com>
2024-01-27 13:34:31 +0300

    update

--- KMS Activator/KMS Activator/Form1.Designer.vb
+++ KMS Activator/KMS Activator/Form1.Designer.vb
@@ -25,7 +25,11 @@ Partial Class Form1
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
         PictureBox1 = New PictureBox()
         Button1 = New Button()
+        Button2 = New Button()
+        PictureBox2 = New PictureBox()
+        ListBox1 = New ListBox()
         CType(PictureBox1, ComponentModel.ISupportInitialize).BeginInit()
+        CType(PictureBox2, ComponentModel.ISupportInitialize).BeginInit()
         SuspendLayout()
         ' 
         ' PictureBox1
@@ -40,18 +44,49 @@ Partial Class Form1
         ' 
         ' Button1
         ' 
-        Button1.Location = New Point(9, 102)
+        Button1.Location = New Point(8, 112)
         Button1.Name = "Button1"
         Button1.Size = New Size(144, 23)
         Button1.TabIndex = 4
         Button1.Text = "Activate Windows 11/10"
         Button1.UseVisualStyleBackColor = True
         ' 
+        ' Button2
+        ' 
+        Button2.Location = New Point(159, 112)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(144, 23)
+        Button2.TabIndex = 6
+        Button2.Text = "Activate Office 2021"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' PictureBox2
+        ' 
+        PictureBox2.Image = CType(resources.GetObject("PictureBox2.Image"), Image)
+        PictureBox2.Location = New Point(190, 12)
+        PictureBox2.Name = "PictureBox2"
+        PictureBox2.Size = New Size(83, 84)
+        PictureBox2.SizeMode = PictureBoxSizeMode.Zoom
+        PictureBox2.TabIndex = 5
+        PictureBox2.TabStop = False
+        ' 
+        ' ListBox1
+        ' 
+        ListBox1.FormattingEnabled = True
+        ListBox1.ItemHeight = 15
+        ListBox1.Location = New Point(8, 141)
+        ListBox1.Name = "ListBox1"
+        ListBox1.Size = New Size(295, 34)
+        ListBox1.TabIndex = 7
+        ' 
         ' Form1
         ' 
         AutoScaleDimensions = New SizeF(7F, 15F)
         AutoScaleMode = AutoScaleMode.Font
-        ClientSize = New Size(160, 136)
+        ClientSize = New Size(311, 181)
+        Controls.Add(ListBox1)
+        Controls.Add(Button2)
+        Controls.Add(PictureBox2)
         Controls.Add(Button1)
         Controls.Add(PictureBox1)
         FormBorderStyle = FormBorderStyle.FixedSingle
@@ -60,10 +95,14 @@ Partial Class Form1
         Name = "Form1"
         Text = "KMS Activator"
         CType(PictureBox1, ComponentModel.ISupportInitialize).EndInit()
+        CType(PictureBox2, ComponentModel.ISupportInitialize).EndInit()
         ResumeLayout(False)
     End Sub
 
     Friend WithEvents PictureBox1 As PictureBox
     Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents ListBox1 As ListBox
 
 End Class
--- KMS Activator/KMS Activator/Form1.vb
+++ KMS Activator/KMS Activator/Form1.vb
@@ -1,35 +1,71 @@
-﻿Public Class Form1
+﻿Imports System.IO
+
+Public Class Form1
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        If (RunCommandCom("slmgr", "/ipk W269N-WFGWX-YVC9B-4J6C9-T83GX", False) = True) Then
-            If (RunCommandCom("slmgr", "/skms kms.digiboy.ir", False) = True) Then
-                If (RunCommandCom("slmgr", "/ato", False) = True) Then
-                    MsgBox("Windows Activated")
-                Else
-                    MsgBox("Activation Failled")
+        If (RunSlmgr("/ipk W269N-WFGWX-YVC9B-4J6C9-T83GX") = True) Then
+            ListBox1.Items.Add("License Installed")
+            If (RunSlmgr("/skms kms.digiboy.ir") = True) Then
+                If (RunSlmgr("/ato")) Then
+                    ListBox1.Items.Add("Microsoft Windows Activated")
                 End If
-            Else
-                MsgBox("Activation Failled")
             End If
-        Else
-            MsgBox("Activation Failled")
+        End If
+    End Sub
+    Public Function RunSlmgr(ByVal cmd As String) As Boolean
+        Dim pi As New ProcessStartInfo With {
+        .Arguments = $"/nologo slmgr.vbs {cmd}",
+        .FileName = "cscript.exe",
+        .RedirectStandardOutput = True,
+        .UseShellExecute = False,
+        .CreateNoWindow = True,
+        .WorkingDirectory = "C:\Windows\System32"
+        }
+
+        Dim p As New Process() With {.StartInfo = pi}
+
+        p.Start()
+        p.WaitForExit()
+
+        Return (p.ExitCode = 0)
+    End Function
+    Public Function RunOspp(ByVal cmd As String) As Boolean
+        Dim pi As New ProcessStartInfo With {
+        .Arguments = $"/nologo ospp.vbs {cmd}",
+        .FileName = "cscript",
+        .RedirectStandardOutput = True,
+        .UseShellExecute = False,
+        .CreateNoWindow = True,
+        .WorkingDirectory = "C:\Program Files\Microsoft Office\Office16"
+        }
+
+        Dim p As New Process() With {.StartInfo = pi}
+
+        p.Start()
+        p.WaitForExit()
+
+        Return (p.ExitCode = 0)
+    End Function
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        For Each foundFile As String In My.Computer.FileSystem.GetFiles("C:\Program Files\Microsoft Office\root\Licenses16", FileIO.SearchOption.SearchAllSubDirectories, "ProPlus2021VL_KMS*.xrm-ms")
+            RunOspp($"/inslic:""{foundFile}""")
+        Next
+
+        ListBox1.Items.Add("License Installed")
+
+        If (RunOspp("/setprt:1688")) Then
+            If (RunOspp("/unpkey:6F7TH >nul")) Then
+                If (RunOspp("/inpkey:FXYTK-NJJ8C-GB6DW-3DYQT-6F7TH")) Then
+                    If (RunOspp("/sethst:107.175.77.7")) Then
+                        If (RunOspp("/act")) Then
+                            ListBox1.Items.Add("Office Activated")
+                        End If
+                    End If
+                End If
+            End If
         End If
     End Sub
 
-    Public Function RunCommandCom(command As String, arguments As String, permanent As Boolean) As Boolean
-        Dim p As Process = New Process()
-        Dim pi As ProcessStartInfo = New ProcessStartInfo()
-        pi.Arguments = " " + If(permanent = True, "/K", "/C") + " " + command + " " + arguments
-        pi.FileName = "cmd.exe"
-        p.StartInfo = pi
-        pi.WindowStyle = ProcessWindowStyle.Hidden
-        p.Start()
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
-        p.WaitForExit()
-
-        If (p.ExitCode = 0) Then
-            Return True
-        Else
-            Return False
-        End If
-    End Function
+    End Sub
 End Class
